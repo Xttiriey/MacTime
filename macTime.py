e542f99eb3958926dfe073a447d41fafd3dc0a0f
Xttiriey <65865283+Xttiriey@users.noreply.github.com>
2020-05-25 14:34:19 +0300

    Update macTime.py

--- macTime.py
+++ macTime.py
@@ -35,6 +35,7 @@ euid = os.geteuid()
 if euid != 0:
     print("Script not started as root. Running script with sudo..")
 else:
+    print("[*] The script will start working after " + options.timeInv + " seconds")
     while True:
         changemac(options.interface)
         time.sleep(float(options.timeInv))  
